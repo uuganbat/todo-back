18dcfd84cab66651711f13ef077e6cf6d76d6e44
bumanit <uuganbat@bumanit.mn>
2021-09-11 19:53:26 +0800

    [FIX] Static Files Path

--- app/settings/common.py
+++ app/settings/common.py
@@ -127,7 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = BASE_DIR / "static"
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (BASE_DIR / "static",)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
