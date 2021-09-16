0877db5d348d42fa68ed9ed0eba038ab99c5754b
bumanit <uuganbat@bumanit.mn>
2021-09-16 22:14:03 +0800

    [ADD] Postgresql

--- app/settings/production.py
+++ app/settings/production.py
@@ -1,5 +1,20 @@
 from .common import *
-
+import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://todo-uit.heroku.com"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todo-uit",
+        "USER": "todo-uit-user",
+        "PASSWORD": "todo-uit-password",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
